f13a86b48ee4e846c53ea374b33f92495757c52a
hello-spring <jiho.develop@gmail.com>
2021-12-29 14:21:53 +0900

    todotest

---
+++ src/test/java/org/springframework/samples/petclinic/todo/domain/TodoTest.java
@@ -0,0 +1,38 @@
+package org.springframework.samples.petclinic.todo.domain;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import java.util.Locale;
+import java.util.Set;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validator;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.samples.petclinic.todo.Todo;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+class TodoTest {
+
+    private Validator createValitor() {
+        LocalValidatorFactoryBean localValidatorFactoryBean = new LocalValidatorFactoryBean();
+        localValidatorFactoryBean.afterPropertiesSet();
+        return localValidatorFactoryBean;
+    }
+
+    @Test
+    void shouldNotValidateWhenNameEmpty() {
+        LocaleContextHolder.setLocale(Locale.KOREA);
+        Todo todo1 = new Todo();
+        todo1.setUsername("");
+
+        Validator validator = createValitor();
+        Set<ConstraintViolation<Todo>> constraintViolations = validator.validate(todo1);
+
+        assertThat(constraintViolations.size()).isEqualTo(1);
+        ConstraintViolation<Todo> violation = constraintViolations.iterator().next();
+        assertThat(violation.getPropertyPath()).hasToString("description");
+        assertThat(violation.getMessage()).hasToString("널이어서는 안됩니다");
+
+    }
+
+}
---
+++ src/test/java/org/springframework/samples/petclinic/todo/service/MybatisTodoServiceImplTest.java
@@ -0,0 +1,80 @@
+package org.springframework.samples.petclinic.todo.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.samples.petclinic.product.ProductDataHandlerImpl;
+import org.springframework.samples.petclinic.product.ProductServiceImpl;
+import org.springframework.samples.petclinic.todo.MybatisTodoServiceImpl;
+import org.springframework.samples.petclinic.todo.Todo;
+import org.springframework.samples.petclinic.todo.TodoMapper;
+import org.springframework.samples.petclinic.todo.TodoService;
+import org.springframework.transaction.annotation.Transactional;
+
+// MockitoExtension: Mockito framework를 juni5에 통합
+// @Mock : create 모의객체
+// @InjectMocks : create 객체 and  모의 의존성 주입
+
+@Transactional
+//@ExtendWith(MockitoExtension.class)
+@SpringBootTest//(classes = {MybatisTodoServiceImpl.class, TodoMapper.class})
+class MybatisTodoServiceImplTest {
+    Logger logger = LoggerFactory.getLogger(MybatisTodoServiceImplTest.class);
+    //@InjectMocks
+    @Autowired
+    private TodoService todoService;
+    //private TodoService todoService = new MybatisTodoServiceImpl();
+    @Autowired
+    private TodoMapper todoMapper;
+
+    @Test
+    @DisplayName("테스트설명")
+    void when_then() throws Exception {
+        // given
+
+        // when
+
+        // then
+     }
+
+
+
+    @Test
+    @DisplayName("todoService 생성확인")
+    void whenTodoServiceInjected_thenNotNull() {
+        assertThat(todoService).isNotNull();
+        assertThat(todoMapper).isNotNull();
+    }
+
+    @Test
+    @DisplayName("사용자 이름으로 조회")
+    void whenGetTodosByUserSuccess_thenCorrectResponse() {
+        // given
+        List<Todo> todoList = new ArrayList<>();
+        final Todo todo1 = new Todo();
+        todo1.setId(1L);
+        todo1.setUsername("arori tester");
+        todo1.setDescription("mybatis 연동 테스트");
+        todo1.setTargetDate(new Date());
+        todoList.add(todo1);
+        todoService.saveTodo(todo1);
+        //given(todoMapper.getTodosByUser(todo1.getUsername())).willReturn(todoList);
+
+        // when
+        List<Todo> resultList= todoMapper.getTodosByUser(todo1.getUsername());
+        logger.info("Username: {}", todoList.get(0).getUsername());
+        // then
+        assertThat(todo1.getUsername()).isEqualTo(resultList.get(0).getUsername());
+        //verify(todoMapper).getTodosByUser(any(String.class));
+    }
+
+
+}
